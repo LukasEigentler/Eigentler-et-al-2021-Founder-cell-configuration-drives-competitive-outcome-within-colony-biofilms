c70ea6786fa09bd3d0d4bf8a137c28578aa4d994
LukasEigentler <67065710+LukasEigentler@users.noreply.github.com>
2021-06-21 13:51:35 +0100

    Update u0fun_equal.m

--- Model_simulations/u0fun_equal.m
+++ Model_simulations/u0fun_equal.m
@@ -5,8 +5,9 @@ function uinit = u0fun_equal(location)
 % spatial mesh based on the locations of the mesh's nodes given by the
 % location vector
 
-% $Author: Lukas Eigentler
-% Last updated: 08/06/2021
+% Author: Lukas Eigentler (leigentler001@dundee.ac.uk)
+% License: GNU GPL
+% Last updated: 21/06/2021
 
 global kcap
 M = length(location.x);
